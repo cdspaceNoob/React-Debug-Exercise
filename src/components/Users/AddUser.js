89dc02ce1243864c15b4949df68830828d7abddc
voyager <cdspacenoob@gmail.com>
2023-01-18 15:06:10 +0900

    useRef()

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
+import { useRef } from "react";
 
 import classes from "./AddUser.module.css";
 import Card from "../../UI/Card";
@@ -7,13 +9,21 @@ import ErrorModal from "../../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState("");
-    const [enteredAge, setEnteredAge] = useState("");
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
     const [error, setError] = useState();
+
+    /* useRef()를 사용함에 따라, state로 관리하던 모든 것들을 주석 처리. */
+    // const [enteredUsername, setEnteredUsername] = useState("");
+    // const [enteredAge, setEnteredAge] = useState("");
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (enteredUsername.length === 0 || enteredAge.length === 0) {
+
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+
+        if (enteredName.length === 0 || enteredUserAge.length === 0) {
             // alert("check your name or age");
             setError({
                 title: "invalid username input",
@@ -23,7 +33,7 @@ const AddUser = (props) => {
         }
         // enteredAge는 초깃값이 String으로 들어갔으나, 비교 시 자동으로 Casting된다.
         // 하지만 더욱 안전을 기하기 위해 + 연산자를 앞에 사용하여 Number 타입으로의 암묵적 변환이 선행되도록 유도한다. 
-        if (+enteredAge < 1) {
+        if (+enteredUserAge < 1) {
             // alert("age must bigger than 1");
             setError({
                 title: "invalid age input",
@@ -31,19 +41,21 @@ const AddUser = (props) => {
             });
             return;
         }
-        props.onAddUser(enteredUsername, enteredAge);
-        console.log(enteredUsername, enteredAge);
-        setEnteredAge("");
-        setEnteredUsername("");
+        props.onAddUser(enteredName, enteredUserAge);
+        console.log(enteredName, enteredUserAge);
+        nameInputRef.current.value = "";
+        ageInputRef.current.value = "";
+        // setEnteredAge("");
+        // setEnteredUsername("");
     };
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    };
+    // const usernameChangeHandler = (event) => {
+    //     setEnteredUsername(event.target.value);
+    // };
 
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    };
+    // const ageChangeHandler = (event) => {
+    //     setEnteredAge(event.target.value);
+    // };
 
     const errorHandler = () => {
         setError(null);
@@ -59,9 +71,21 @@ const AddUser = (props) => {
                     {/* JSX에서는 label for 대신 htmlFor를 사용한다. */}
                     <label htmlFor="username" >Username</label>
                     {/* JSX에서도 id를 정해줄 수 있다. */}
-                    <input id="username" type="text" onChange={usernameChangeHandler} value={enteredUsername} />
+                    <input
+                        id="username"
+                        type="text"
+                        // onChange={usernameChangeHandler}
+                        // value={enteredUsername}
+                        ref={nameInputRef}
+                    />
                     <label htmlFor="age">Age(Years)</label>
-                    <input id="age" type="number" onChange={ageChangeHandler} value={enteredAge} />
+                    <input
+                        id="age"
+                        type="number"
+                        // onChange={ageChangeHandler}
+                        // value={enteredAge}
+                        ref={ageInputRef}
+                    />
                     {/* 커스텀 컴포넌트의 이름이 Button이다. props를 자연스럽게 전달한다. */}
                     <Button type="submit">Add User</Button>
                 </form>
